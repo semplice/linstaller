2a05395a4cafe9e0208e07e8f23672338a11df78
g7 <morarossa@gmail.com>
2011-08-20 00:12:33 +0200

    partdisks: do not format if in interactive editor, swap is selected but swap_noformat preseed is true.

--- linstaller/modules/partdisks/front.py
+++ linstaller/modules/partdisks/front.py
@@ -303,6 +303,9 @@ class CLIFrontend(cli.CLIFrontend):
 								
 				# Should format?
 				if "format_real" in cng:
+					# If this is going to be the swap partition, if swap_nofromat is setted, we should not format.
+					if "useas" in cng and cng["useas"] == "swap" and self.settings["swap_noformat"]:
+						continue
 					# Yes.
 					progress = lib.format_partition_for_real(obj, cng["format_real"])
 					info(_("Formatting %s...") % key)
