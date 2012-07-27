e492315ea13356028a3339b7af11ceaa5411110f
Eugenio Paolantonio <me@medesimo.eu>
2012-07-27 11:05:01 +0200

    modules/bootloader: ported to the glade frontend.

---
+++ linstaller/modules/bootloader/front/glade/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# linstaller bootloader module frontend - (C) 2011 Eugenio "g7" Paolantonio and the Semplice Team.
+# All rights reserved. Work released under the GNU GPL license, version 3 or later.
+#
+# This is a module of linstaller, should not be executed as a standalone application.
+
+import linstaller.frontends.glade as glade
+import t9n.library
+_ = t9n.library.translation_init("linstaller")
+
+from linstaller.core.main import warn,info,verbose
+
+class Frontend(glade.Frontend):
+	def ready(self):
+		
+		self.set_header("info", _("Bootloader"), _("Bootloader settings"))
+		
+		if (self.is_module_virgin and self.settings["device"] != False) or self.settings["caspered"] == True:
+			# Selected device, do not prompt.
+			if self.is_module_virgin:
+				# Ensure we say that on the virgin state casper has been executed
+				self.settings["caspered"] = True
+			self.module_casper()
+				
+		if not self.is_module_virgin:
+			self.set_header("ok", _("You can continue!"), _("Press forward to continue."))
+			return
+
+		# Get text label
+		text = self.objects["builder"].get_object("text")
+		
+		# Format label:
+		label = (
+			_("The bootloader is that piece of software that lets you boot your %(distroname)s system.") % {"distroname":self.moduleclass.main_settings["distro"]},
+			_("Without the bootloader, you can't boot %(distroname)s.") % {"distroname":self.moduleclass.main_settings["distro"]},
+			"",
+			_("You can choose to install the bootloader into the Master Boot Record of your hard disk. This is recommended."),
+			_("If you do not want to install the bootloader into the MBR, it will be installed on your root partition."),
+			"",
+		)
+
+		# Properly set it
+		text.set_markup("\n".join(label))
+		
+		# Get the checkbox
+		self.onmbr = self.objects["builder"].get_object("onmbr")
+	
+	def on_next_button_click(self):
+		""" Proper set settings["device"] when Next button has been clicked. """
+		
+		value = self.onmbr.get_active()
+		if value:
+			# Yay
+			self.settings["device"] = "mbr"
+		else:
+			# Root
+			self.settings["device"] = "root"
+			
+		verbose("Bootloader %s will be installed in %s" % (self.settings["bootloader"], self.settings["device"]))
+		
+		return None
---
+++ linstaller/modules/bootloader/inst/glade/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# linstaller bootloader module install - (C) 2011 Eugenio "g7" Paolantonio and the Semplice Team.
+# All rights reserved. Work released under the GNU GPL license, version 3 or later.
+#
+# This is a module of linstaller, should not be executed as a standalone application.
+
+import linstaller.core.main as m
+import linstaller.frontends.glade as glade
+import t9n.library
+_ = t9n.library.translation_init("linstaller")
+
+from linstaller.core.main import warn,info,verbose
+
+class BootloaderInstall(glade.Progress):
+	def progress(self):
+		
+		self.parent.progress_wait_for_quota()
+
+		# Get bootloader
+		bootloader = self.parent.moduleclass.modules_settings["bootloader"]["bootloader"]
+
+		m.verbose("Installing %s bootloader..." % bootloader)
+
+		try:
+			# PASS 1: INSTALL
+			self.parent.progress_set_text(_("Installing bootloader..."))
+			self.parent.moduleclass._install[bootloader]()
+			self.parent.progress_set_percentage(1)
+			
+			# PASS 2: UPDATE
+			self.parent.progress_set_text(_("Generating bootloader configuration..."))
+			self.parent.moduleclass._update[bootloader]()
+			self.parent.progress_set_percentage(2)
+		finally:
+			# Exit
+			self.parent.moduleclass.install.close()
+
+
+class Frontend(cli.Frontend):
+	def ready(self):
+		""" Start the frontend """
+		
+		self.progress_set_quota(2)
+	
+	def process(self):
+		""" Process install """
+		
+		clss = BootloaderInstall(self)
+		clss.start()
