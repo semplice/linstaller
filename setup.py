383acc1551421dcd77c92cf2f85f80cb144a5955
Eugenio Paolantonio (g7) <me@medesimo.eu>
2013-12-23 12:50:53 +0100

    Release 5.23.0

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ data_files = [
 data_files += data
 
 distrib = setup(name='linstaller',
-      version='5.22.2',
+      version='5.23.0',
       description='Modular, preseedable, GNU/Linux distribution installer',
       author='Eugenio Paolantonio and the Semplice Team',
       author_email='me@medesimo.eu',
