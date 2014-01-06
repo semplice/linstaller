e0b32f62c0e7a39bf0305e8b2e9cefe49682d1af
Eugenio Paolantonio (g7) <me@medesimo.eu>
2014-01-06 13:03:54 +0100

    Add and ship the linstaller desktop launcher.

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ data, symlinks = search_for_glade()
 data_files = [
 	("/usr/bin", ["linstaller_wrapper.sh", "linstaller_crash_wrapper.sh", "session/linstaller_session.sh", "mount_nolive.sh"]),
 	("/etc/linstaller", ["config/semplice", "config/semplice.glade", "config/semplice-base", "config/semplice-nolive", "config/semplice-nolive.glade", "config/ubuntu", "config/ubuntu-nolive", "config/semplice-persistent", "config/semplice-persistent-nolive", "config/semplice-raspberrypi", "config/semplice-fullscreen", "config/semplice-fullscreen.glade"]),
+	("/usr/share/applications", ["linstaller.desktop"]),
 	("/usr/share/xsessions", ["session/linstaller.desktop"]),
 ]
 data_files += data
