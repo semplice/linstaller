237fc482d7cba95fc802958f2d398c7243398155
Eugenio Paolantonio (g7) <me@medesimo.eu>
2014-01-14 19:13:36 +0100

    5.80.1

--- linstaller/core/main.py
+++ linstaller/core/main.py
@@ -5,7 +5,7 @@
 # This is a module of linstaller, should not be executed as a standalone application.
 
 ####
-VERSION = "5.80.0"
+VERSION = "5.80.1"
 ####
 
 import os, sys, traceback
--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ data_files = [
 data_files += data
 
 distrib = setup(name='linstaller',
-      version='5.80.0',
+      version='5.80.1',
       description='Modular, preseedable, GNU/Linux distribution installer',
       author='Eugenio Paolantonio and the Semplice Team',
       author_email='me@medesimo.eu',
