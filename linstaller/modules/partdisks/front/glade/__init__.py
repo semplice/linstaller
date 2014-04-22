semplice/linstaller

commit 77371d5fcd043fd7685f486795027796105519d5

modules/partdisks: front/glade: on manual_frame_creator() prefer device objects in self.changed.
If the device is already in self.changed, we should take the objects directly from it, otherwise
bad things will happen when committing things (e.g. new partitions will not be physically created).
I fighted with this for months, and in the end it's only a three line fix. Wow.

linstaller/modules/partdisks/front/glade/__init__.py
@@ -2136,7 +2136,7 @@ class Frontend(glade.Frontend):
 				else:
 					# Ok!
 					self.set_header("hold", _("You have some unsaved changes!"), _("Use the Apply button to save them."))
-
+					
 				if not self.get_device_from_selected().path in self.touched: self.touched.append(self.get_device_from_selected().path)
 				# Remove changes
 				val = lib.return_device(self.current_selected["value"])
@@ -2543,7 +2543,17 @@ class Frontend(glade.Frontend):
 	def manual_frame_creator(self, device, disk, on_lvm=False):
 		""" Creates frames etc for the objects passed. """
 		
-		if not device.path in self.changed: self.changed[str(device.path)] = {"obj":device, "disk":disk, "changes":{}}
+		if not device.path in self.changed:
+			self.changed[str(device.path)] = {"obj":device, "disk":disk, "changes":{}}
+		else:
+			# If the device is already in changed, we should take the objects
+			# directly from it, otherwise bad things will happen when
+			# committing things (e.g. new partitions will not be physically created)-
+			# Note to self: the next time you create a partition manager
+			# ensure to have an unique pool where store all device objects.
+			# Thanks.
+			device = self.changed[str(device.path)]["obj"]
+			disk = self.changed[str(device.path)]["disk"]
 		
 		container = {}
 		container["frame_label"] = Gtk.Label()

commit 7b2ba244c6df2b03de1c73d21e64925f3a3aaf20

modules/partdisks/front/glade: automatic_calc(): when setting up self.changed in order to switch to the manual summary scene, properly store the device and disk objects.
self.changed[diskpath]["obj"] was a Disk object, instead of a Device. Disks should be associated with the "disk" key.
This broke the automatic mode after 77371d5fcd043fd7685f486795027796105519d5, as manual_frame_creator() now expects a Device object (to retrieve its path).
This commit should also fix the intermittent issues with the automatic partitioner.
Thanks again to Filip Danilovic for the bug report.

linstaller/modules/partdisks/front/glade/__init__.py
@@ -851,7 +851,7 @@ class Frontend(glade.Frontend):
 			if refreshlvm: lvm.refresh()
 		
 		diskpath = res["device"].path
-		self.changed[diskpath] = {"changes":{}, "obj":res["disk"], "fromAutomatic":True}
+		self.changed[diskpath] = {"changes":{}, "obj":res["device"], "disk":res["disk"], "fromAutomatic":True}
 		self.touched.append(diskpath)
 		self.previously_changed.append(diskpath)
 		